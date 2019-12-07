5f8b3d2488da81f5482a8bd93ee1852ae18ed097
Kevin Wenger <wenger.kev@gmail.com>
2019-12-07 13:46:06 +0100

    add Team Member field

---
+++ web/modules/custom/gos_site/src/Plugin/Field/FieldFormatter/TeamMemberFieldFormatter.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\gos_site\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\Plugin\Field\FieldFormatter\EntityReferenceLabelFormatter;
+
+/**
+ * Plugin implementation of the 'team_member_default' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "team_member_default",
+ *   label = @Translation("Default"),
+ *   field_types = {
+ *     "team_member"
+ *   }
+ * )
+ */
+class TeamMemberFieldFormatter extends EntityReferenceLabelFormatter {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    // Does not actually output anything.
+    return [];
+  }
+
+}
