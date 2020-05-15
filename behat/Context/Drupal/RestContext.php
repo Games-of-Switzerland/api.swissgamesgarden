2d380f0c3a01434123f9888091c2cf313f765195
Kevin Wenger <wenger.kev@gmail.com>
2020-05-15 17:50:13 +0200

    add PHPStan

--- behat/Context/Drupal/RestContext.php
+++ behat/Context/Drupal/RestContext.php
@@ -12,10 +12,10 @@ class RestContext extends ApiContext {
   /**
    * Set a CSRF Token for the next request.
    *
-   * @Then generate the CSRF Token request header
+   * @Then I add the CSRF Token request header
    */
   public function setCsrfToken() {
-    $request = $this->client->request('GET', '/rest/session/token?_format=json');
+    $request = $this->client->request('GET', '/session/token?_format=json');
     $token = $request->getBody()->getContents();
     $this->request = $this->request->withHeader('x-csrf-token', $token);
   }
