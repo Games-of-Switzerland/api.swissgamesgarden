cb0db28a4e87588054a0e5e491039c4e73e82c08
Kevin Wenger <wenger.kev@gmail.com>
2020-06-02 16:18:59 +0200

    increase the limit of aggregated bucket from 10 to 100 items

--- web/modules/custom/gos_elasticsearch/src/Plugin/rest/resource/ElasticGamesResource.php
+++ web/modules/custom/gos_elasticsearch/src/Plugin/rest/resource/ElasticGamesResource.php
@@ -149,7 +149,7 @@ class ElasticGamesResource extends ElasticResourceBase {
                     ],
                     'aggregations' => [
                       'genres_name_keyword' => [
-                        'terms' => ['field' => 'genres.name_keyword', 'min_doc_count' => 0],
+                        'terms' => ['field' => 'genres.name_keyword', 'min_doc_count' => 0, 'size' => 100],
                       ],
                     ],
                   ],
@@ -169,7 +169,7 @@ class ElasticGamesResource extends ElasticResourceBase {
                     ],
                     'aggregations' => [
                       'releases_platform_keyword' => [
-                        'terms' => ['field' => 'releases.platform_keyword', 'min_doc_count' => 0],
+                        'terms' => ['field' => 'releases.platform_keyword', 'min_doc_count' => 0, 'size' => 100],
                       ],
                     ],
                   ],
