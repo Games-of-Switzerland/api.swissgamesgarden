f9a5a0c45780235b9421a305db4205cf4a313f7e
Kevin Wenger <wenger.kev@gmail.com>
2020-09-03 18:36:36 +0200

    display openssl on travis

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -9,6 +9,7 @@
 
 scriptDir=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 
+openssl version
 openssl aes-256-cbc -k "$DEPLOY_KEY" -in "$TRAVIS_BUILD_DIR/config/deploy_id_rsa_enc_travis" -d -a -out "$TRAVIS_BUILD_DIR/config/deploy_id_rsa"
 gem install bundler
 bundle install
