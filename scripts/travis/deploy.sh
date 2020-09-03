62e5fc5844619d213e63640afcc317986938c090
Kevin Wenger <wenger.kev@gmail.com>
2020-09-03 18:20:35 +0200

    decrypt the id_rsa to be used on deployment

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -9,6 +9,7 @@
 
 scriptDir=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 
+openssl aes-256-cbc -k $DEPLOY_KEY -in config/deploy_id_rsa_enc_travis -d -a -out config/deploy_id_rsa
 gem install bundler
 bundle install
 bundle exec cap "$1" deploy
